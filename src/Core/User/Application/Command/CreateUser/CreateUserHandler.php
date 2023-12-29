015bc3a3466f199ba0572c1db4153979aa74968d
Mykhailo Dvoretskyi <m.dvoretskiy@gmail.com>
2023-12-29 18:15:56 +0200

    sending an email to the user after registration

--- src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
+++ src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Core\User\Application\Command\CreateUser;
 
+use App\Common\Mailer\MailerInterface;
 use App\Core\User\Domain\Repository\UserRepositoryInterface;
 use App\Core\User\Domain\User;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -10,7 +11,8 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 class CreateUserHandler
 {
     public function __construct(
-        private readonly UserRepositoryInterface $userRepository
+        private readonly UserRepositoryInterface $userRepository,
+        private readonly MailerInterface $mailer
     ) {}
 
     public function __invoke(CreateUserCommand $command): void
@@ -18,5 +20,11 @@ class CreateUserHandler
         $this->userRepository->save(new User($command->email));
 
         $this->userRepository->flush();
+
+        $this->mailer->send(
+            $command->email,
+            "Rejestracja użytkownika",
+            "Zarejestrowano konto w systemie. Aktywacja konta trwa do 24h"
+        );
     }
 }
